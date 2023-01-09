d039e0d32ecadb868ec0fde6ab88877fce0620f8
Shinni <69644134+ShinniUwU@users.noreply.github.com>
2023-01-09 19:42:45 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,18 +1,46 @@
-$(document).ready(function() {
+window.onload = function() {
+  // Get the video element
+  var video = document.getElementById("myVideo");
 
-var typed = new Typed('#typed',{
-    strings:["GHOST"],
-    backSpeed: 40,
-    typeSpeed: 200,
-    showCursor: false
+  // Set up the flag colors
+  const BLACK = "#000000";
+  const YELLOW = "#FFD500";
+  const BLUE = "#005BBB";
+  const RED = "#D10000";
+
+  // Handle cases where the video element or the document.body element is null
+  if (video === null) {
+    console.error("Unable to find video element with ID 'myVideo'");
+    return;
+  }
+  if (document.body === null) {
+    console.error("Unable to find body element");
+    return;
+  }
+
+  // Set the initial background color of the flag to black
+  document.body.style.backgroundColor = BLACK;
+
+  // Add an event listener for the "play" event on the video element
+  video.addEventListener("play", function() {
+    // Set up the breathing effect interval
+    var interval = setInterval(function() {
+      // Fade the background color to yellow using a CSS transition
+      document.body.style.transition = "background-color 1s ease-in-out";
+      document.body.style.backgroundColor = YELLOW;
+      // Wait 1 second
+      setTimeout(function() {
+        // Fade the background color to blue using a CSS transition
+        document.body.style.transition = "background-color 1s ease-in-out";
+        document.body.style.backgroundColor = BLUE;
+      }, 1000);
+    }, 2000);
   });
 
-}); 
-var i = 0;
-function change() {
-  var doc = document.getElementById("background");
-  var color = ["blue", "yellow"];
-  doc.style.backgroundColor = color[i];
-  i = (i + 1) % color.length;
+  // Add an event listener for the "pause" event on the video element
+  video.addEventListener("pause", function() {
+    // Clear the breathing effect interval
+    clearInterval(interval);
+    // Fade the background color to black using a CSS
+  });
 }
-setInterval(change, 1000);
